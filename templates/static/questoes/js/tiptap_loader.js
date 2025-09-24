43889da84165dc446c317eb5e645276989bd0c80
Yuri da Silva Ferreira <142122885+Yuridevpro@users.noreply.github.com>
2025-09-24 20:58:50 -0300

    atualizações 25

--- templates/static/questoes/js/tiptap_loader.js
+++ templates/static/questoes/js/tiptap_loader.js
@@ -1,12 +1,11 @@
 import { Editor } from 'https://esm.sh/@tiptap/core';
 import StarterKit from 'https://esm.sh/@tiptap/starter-kit';
 import { Markdown } from 'https://esm.sh/tiptap-markdown';
-import Underline from 'https://esm.sh/@tiptap/extension-underline';
 
 // Anexa tudo ao objeto global 'window' para ser acessível no SCRIPT de inicialização
+// A extensão 'Underline' foi removida, pois já está incluída no StarterKit.
 window.TipTap = {
     Editor,
     StarterKit,
     Markdown,
-    Underline,
 };
