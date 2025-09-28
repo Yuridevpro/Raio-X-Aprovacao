b457f959acf50808f05bd98130b36acc84f3022d
Yuri da Silva Ferreira <142122885+Yuridevpro@users.noreply.github.com>
2025-09-27 22:48:22 -0300

    atualizações 47

--- qconcurso/settings.py
+++ qconcurso/settings.py
@@ -179,15 +179,24 @@ MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
 
 
 
-#  usa um servidor SMTP real
+# #  usa um servidor SMTP real
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com' # Exemplo para Gmail
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com' # Exemplo para Gmail
+EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = 'apikey'  # Esta palavra é literal e obrigatória.
+EMAIL_HOST_PASSWORD = os.getenv('SENDGRID_API_KEY') 
 
-
+# Define o remetente padrão para todos os e-mails.
+# Este valor DEVE corresponder ao seu "Single Sender" verificado no SendGrid.
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
 
 # --- OUTRAS CONFIGURAÇÕES ---
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
